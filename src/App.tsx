f0e6389526befa26cc1f899e91ff10967f8e47a5
rosieieio <182373827+rosieieio@users.noreply.github.com>
2026-05-19 19:54:10 -0400

    Add basic todo list components and cleanup folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
 
 function App() {
-  
 
   return (
-    <></>
-  )
+    <div>
+      <h1 className="title">Todo List</h1>
+      <form className="add-todo-form">
+        <input type="text" name="Add your task" />
+        <button type="submit">Add</button>
+      </form>
+
+      <ul>
+        <li>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App
