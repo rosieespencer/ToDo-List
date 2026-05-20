469a88269e6edb145476be0e9ff0993b2182e709
rosieieio <182373827+rosieieio@users.noreply.github.com>
2026-05-19 23:26:05 -0400

    Adds strikethrough effect to text for checked off tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,35 @@
 import { useState } from 'react'
 
 function App() {
-
   // Todo list item shape
-  type Todo = {
-    id: number;
-    task: string;
-    completed: boolean;
-  };
-
+  type Todo = { id: number; task: string; completed: boolean; };
   const [task, setTask] = useState<string>('');
-
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleSubmit = (e: React.SubmitEvent) => {
     e.preventDefault();
     
     // Check task is not empty
-    if (!task.trim().length) {
-      return;
-    };
+    if (!task.trim().length) return;
 
     // Create Todo list item
-    const newTodo: Todo = {
-      id: Date.now(),
-      task: task,
-      completed: false,
-    };
+    const newTodo: Todo = { id: Date.now(), task: task, completed: false };
 
     // Add new todo to list
     setTodos([...todos, newTodo]);
     setTask('');
-  }
+  };
 
   const handleDelete = (id: number) => {
     const newTodos = todos.filter(todo => todo.id !== id);
     setTodos(newTodos);
   }
+
+  const handleCheckedBox = (id: number) => {
+    setTodos(todos.map(todo =>
+      todo.id == id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
 
   return (
     <div>
@@ -48,14 +41,20 @@ function App() {
           onChange={(e) => setTask(e.target.value)}
           placeholder="Add your task..."
         />
-        <button type="submit">add</button>
+        <button className="submit" type="submit">add</button>
       </form>
 
       <ul>
         {todos.map((todo) => ( 
           <li key={todo.id}>
-            <input type="checkbox" />
-            {todo.task}
+            <input className="checkbox" 
+              type="checkbox" 
+              checked={todo.completed}
+              onChange={() => handleCheckedBox(todo.id)}
+            />
+            <span className="task" style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.task}
+            </span>
             <button className="delete" onClick={() => handleDelete(todo.id)}>delete</button>
           </li>
         ))}
