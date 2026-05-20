dd76514bdf99434d881bfd6e4a5c899cb2f4e008
rosieieio <182373827+rosieieio@users.noreply.github.com>
2026-05-19 20:41:00 -0400

    Adds ability to add tasks to Todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
+import { useState } from 'react'
 
 function App() {
 
+  // Todo list item shape
+  type Todo = {
+    id: number;
+    task: string;
+    completed: boolean;
+  };
+
+  const [task, setTask] = useState<string>('');
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+    
+    // Check task is not empty
+    if (!task.trim().length) {
+      return;
+    };
+
+    // Create Todo list item
+    const newTodo: Todo = {
+      id: Date.now(),
+      task: task,
+      completed: false,
+    };
+
+    // Add new todo to list
+    setTodos([...todos, newTodo]);
+    setTask('');
+  }
+
   return (
     <div>
-      <h1 className="title">Todo List</h1>
-      <form className="add-todo-form">
-        <input type="text" name="Add your task" />
-        <button type="submit">Add</button>
+      <h1 className="header">Todo List</h1>
+      <form className="add-todo-form" onSubmit={handleSubmit}>
+        <input
+          type="text" 
+          value={task} 
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Add your task..."
+        />
+        <button type="submit">add</button>
       </form>
 
       <ul>
-        <li>
-        </li>
+        {todos.map((todo) => ( 
+          <li key={todo.id}>
+            <input type="checkbox"/>
+            {todo.task}
+          </li>
+        ))}
       </ul>
     </div>
   );
